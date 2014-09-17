b68d3f55de6e3d8ab6b5da5475bde7a85b78e7e1
DehengXu <dehengxu@outlook.com>
2014-09-17 15:03:03 +0800

    Initial Commit

---
+++ Box2d-x/Box2d_x.h
@@ -0,0 +1,13 @@
+//
+//  Box2d_x.h
+//  Box2d-x
+//
+//  Created by DehengXu on 14/9/17.
+//  Copyright (c) 2014年 DehengXu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Box2d_x : NSObject
+
+@end
---
+++ Box2d-x/Box2d_x.m
@@ -0,0 +1,13 @@
+//
+//  Box2d_x.m
+//  Box2d-x
+//
+//  Created by DehengXu on 14/9/17.
+//  Copyright (c) 2014年 DehengXu. All rights reserved.
+//
+
+#import "Box2d_x.h"
+
+@implementation Box2d_x
+
+@end
